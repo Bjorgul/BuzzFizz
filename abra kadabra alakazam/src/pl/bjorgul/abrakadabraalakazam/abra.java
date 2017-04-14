b4a29dacef9650b9429934515d6e79a5de98afad
Bartłomiej <blysk88@gmail.com>
2017-04-14 14:47:40 +0200

    Add files via upload

---
+++ abra kadabra alakazam/src/pl/bjorgul/abrakadabraalakazam/abra.java
@@ -0,0 +1,41 @@
+package pl.bjorgul.abrakadabraalakazam;
+import java.util.Scanner;
+
+public class abra {
+
+	public static void main(String args[]) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		int abra;
+		int kadabra;
+		int liczba;
+
+		System.out.println("Podaj liczbê ABRA");
+		abra = scanner.nextInt();
+		System.out.println(abra + " to liczba ABRA");
+
+		System.out.println("Podaj liczbê KADABRA");
+		kadabra = scanner.nextInt();
+		System.out.println(kadabra + " to liczba KADABRA");
+
+		System.out.println("Do ilu ma sprawdzaæ?");
+		liczba = scanner.nextInt();
+		System.out.println("Bêdzie sprawdzaæ do " + liczba);
+
+		for (int a = 1; a <= liczba; a++) {
+
+			if (a % abra == 0 && a % kadabra == 0) {
+				System.out.println("ALAKAZAM");
+			} else if (a % abra == 0) {
+				System.out.println("ABRA");
+			} else if (a % kadabra == 0) {
+				System.out.println("KADABRA");
+			} else {
+				System.out.println(a);
+			}
+
+			scanner.close();
+		}
+	}
+}
